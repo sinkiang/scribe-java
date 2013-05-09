6c9b328df38b3fd1e7fbe248f787eb9b3ce3abe2
siniangchoi <sinkiangchoi@gmail.com>
2013-05-10 03:20:56 +0800

    add BaiduApi

---
+++ src/main/java/org/scribe/builder/api/BaiduApi.java
@@ -0,0 +1,45 @@
+package org.scribe.builder.api;
+
+import org.scribe.builder.api.DefaultApi20;
+import org.scribe.extractors.AccessTokenExtractor;
+import org.scribe.extractors.JsonTokenExtractor;
+import org.scribe.model.OAuthConfig;
+import org.scribe.model.Verb;
+import org.scribe.utils.OAuthEncoder;
+
+/**
+ * Baidu OAuth 2.0 api.
+ */
+public class BaiduApi extends DefaultApi20 {
+	private static final String AUTHORIZE_URL = "https://openapi.baidu.com/oauth/2.0/authorize?client_id=%s&redirect_uri=%s&response_type=code";
+	private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL
+			+ "&scope=%s";
+
+	@Override
+	public Verb getAccessTokenVerb() {
+		return Verb.POST;
+	}
+
+	@Override
+	public AccessTokenExtractor getAccessTokenExtractor() {
+		return new JsonTokenExtractor();
+	}
+
+	@Override
+	public String getAccessTokenEndpoint() {
+		return "https://openapi.baidu.com/oauth/2.0/token?grant_type=authorization_code";
+	}
+
+	@Override
+	public String getAuthorizationUrl(OAuthConfig config) {
+		// Append scope if present
+		if (config.hasScope()) {
+			return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(),
+					OAuthEncoder.encode(config.getCallback()),
+					OAuthEncoder.encode(config.getScope()));
+		} else {
+			return String.format(AUTHORIZE_URL, config.getApiKey(),
+					OAuthEncoder.encode(config.getCallback()));
+		}
+	}
+}
