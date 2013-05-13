cc9f933d4bba0d99a555f1ad8874aada3f99f295
siniangchoi <sinkiangchoi@gmail.com>
2013-05-13 22:47:08 +0800

    google

---
+++ src/test/java/org/scribe/examples/Google2Example.java
@@ -0,0 +1,66 @@
+package org.scribe.examples;
+
+import java.util.Scanner;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.GoogleApi20;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+
+public class Google2Example {
+	private static final String NETWORK_NAME = "Google 2";
+	private static final String PROTECTED_RESOURCE_URL = "https://docs.google.com/feeds/default/private/full/";
+	private static final Token EMPTY_TOKEN = null;
+
+	public static void main(String[] args) {
+		// Replace these with your own api key and secret
+		String apiKey = "204269748538.apps.googleusercontent.com";
+		String apiSecret = "TD_aDT8DNrmfq_vQZiLdH9a-";
+		OAuthService service = new ServiceBuilder().provider(GoogleApi20.class)
+				.apiKey(apiKey).apiSecret(apiSecret).scope("openid email")
+				.callback("http://localhost/oauth/google/callback").build();
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("=== " + NETWORK_NAME + "'s OAuth Workflow ===");
+		System.out.println();
+
+		// Obtain the Authorization URL
+		System.out.println("Fetching the Authorization URL...");
+		String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
+		System.out.println("Got the Authorization URL!");
+		System.out.println("Now go and authorize Scribe here:");
+		System.out.println(authorizationUrl);
+		System.out.println("And paste the authorization code here");
+		System.out.print(">>");
+		Verifier verifier = new Verifier(in.nextLine());
+		System.out.println();
+
+		// Trade the Request Token and Verifier for the Access Token
+		System.out.println("Trading the Request Token for an Access Token...");
+		Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+		System.out.println("Got the Access Token!");
+		System.out.println("(if your curious it looks like this: "
+				+ accessToken + " )");
+		System.out.println();
+
+		// Now let's go and ask for a protected resource!
+		System.out.println("Now we're going to access a protected resource...");
+		OAuthRequest request = new OAuthRequest(Verb.GET,
+				PROTECTED_RESOURCE_URL);
+		service.signRequest(accessToken, request);
+		Response response = request.send();
+		System.out.println("Got it! Lets see what we found...");
+		System.out.println();
+		System.out.println(response.getCode());
+		System.out.println(response.getBody());
+
+		System.out.println();
+		System.out
+				.println("Thats it man! Go and build something awesome with Scribe! :)");
+
+	}
+}
